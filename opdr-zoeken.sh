896d5076c22c9f8d3563216b13f2a27caa6d14bf
himrsumma <45629437+himrsumma@users.noreply.github.com>
2019-10-02 08:09:54 +0200

    Add files via upload

---
+++ opdr-zoeken.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+mkdir ~/data ~/data/administratie ~/data/algemeen ~/data/personeelszaken ~/data/directie
+cat > ~/data/administratie/administratie.txt <<- "EOF"
+Administratie wil in veel gevallen zeggen: kantoorwerk in de breedste zin van het woord.
+
+Vroeger ging het veelal om het bijhouden van financiële verantwoording door middel van boekhouding bestaande uit onder andere kasboeken, journaaloverzichten. Voorheen gebeurde dat meestal handgeschreven. In de loop van de twintigste eeuw werd de typemachine steeds meer gebruikt en in de jaren negentig deed de computer uitgebreid zijn intreden.
+
+Tegenwoordig betekent het vaak geautomatiseerd databasewerk voor onder andere personeelszaken, zoals uitbetaling van salaris, registratie van vrije dagen, enzovoort. Denk ook aan logistieke zaken en uitkeringsinstanties als het UWV. In de zorgsector wordt gesproken van patiëntenadministratie of medische administratie.
+
+De moeite of kosten voor ondernemers, instellingen en particulieren om overheidsregels na te leven en vergunningen aan te vragen wordt wel aangeduid als administratieve lasten(druk) of regeldruk. Bij bijvoorbeeld een belastingwet, komt deze boven op de last van de belasting zelf; bij een subsidie vormt deze voor de ontvanger de keerzijde. Bij introductie, wijzigen en afschaffen van regelingen is vaak het streven deze lastendruk te verminderen of niet te veel te laten stijgen (zie ook Actal). Zo is er bijvoorbeeld de Wet uniformering loonbegrip en wordt een winstbox en loonsomheffing overwogen. 
+EOF
+cat > ~/data/administratie/salaris.txt <<- "EOF"
+Het is onduidelijk wanneer voor het eerst loon werd betaald. Loon is te zien als een vorm van ruilhandel; arbeid en loon worden tegen elkaar geruild. Arbeid - en dus loon - heeft doorgaans een structurele aard. Om een dergelijke structurele ruilhandel te realiseren, moet een zekere mate van beschavingsontwikkeling hebben plaatsgevonden. Meer specifiek moeten werkgevers aanwezig zijn - wellicht een overheid of een religieus orgaan - die structureel genoeg aanwezig zijn om op zo'n regelmatige basis betalingen voor arbeid te ruilen dat ook daadwerkelijk sprake is van loon. Mogelijk heeft de eerste loon-arbeid-ruilhandel plaatsgevonden tijdens de neolithische revolutie in een stad of dorp ergens tussen 10.000 en 1.000 v.Chr.
+
+Het woord "salaris" betekent letterlijk "zoutrantsoen" en was de betaling van een Romeins soldaat. In gebieden waar zout schaars was, is het lange tijd als betaalmiddel gebruikt.
+
+In Nederland werd van oudsher het begrip "salaris" gebruikt voor het loon van ambtenaren en hogere functionarissen in bedrijven, terwijl het loon van arbeiders "loon" werd genoemd. Salaris werd per jaar, per kwartaal of per maand uitbetaald, loon per week of per dag (dagloners). Sinds de jaren 1960 zijn deze verschillen officieel verdwenen, maar het begrip salaris wordt nog steeds gekoppeld aan een hoog inkomen: de topman van een onderneming ontvangt bijvoorbeeld een "topsalaris" en geen "toploon".
+
+Voor ambtenaren, inclusief militairen, wordt voor loon of salaris ook bezoldiging gebruikt. Voor militairen wordt het loon of de salaris ook wedde genoemd. Dienstplichtigen ontvingen in het verleden soldij. 
+EOF
+
+cat > ~/data/directie/directeur.txt <<- "EOF"
+Directeur of directrice[1] is de algemene benaming voor iemand die de leiding heeft over een organisatie, bedrijf, dienst of afdeling. Deze persoon kan bijvoorbeeld de leiding geven aan een handels- of industriële onderneming, ziekenhuis, school, museum etc. (bijvoorbeeld financieel directeur, personeelsdirecteur, schooldirecteur enz.).
+
+In de handels- en bedrijfswereld hanteert men daarvoor tegenwoordig vaak Engelstalig jargon; men heeft het dan over de CEO (chief executive officer) als men de directeur van een bedrijf bedoelt.
+
+De personen die in een organisatie de directeursfuncties vervullen worden gezamenlijk de directie genoemd. In de ambtelijke wereld spreekt men van directie als synoniem voor de betreffende afdeling. Zo spreekt men bijvoorbeeld van de directie logistiek, de directie personeel, de directie cultuur, of bij de diverse belastingdiensten: eerste, tweede, derde enz. directie.
+
+Een geestelijk directeur is de persoon die verantwoordelijk is voor de geestelijke vorming binnen een instelling.
+
+Het ambt van directeur alsook zijn bureau of kantoor noemt men directoraat. 
+EOF
+cat > ~/data/algemeen/linux.txt <<- "EOF"
+Linux is een open-source-, Unix-achtig besturingssysteem gebaseerd op de Linuxkernel. De verschillende Linuxvarianten worden Linuxdistributies genoemd en zijn meestal gratis verkrijgbaar. De Linuxkernel wordt verspreid onder de GNU General Public License (GPL). Omdat Linuxdistributies in de regel gebruikmaken van het GNU-besturingssysteem, de standaard-C-bibliotheek en voldoen aan de POSIX-standaard, wordt het geheel ook wel GNU/Linux genoemd. 
+EOF
+cat > ~/data/algemeen/geschiedenis-linux.txt <<- "EOF"
+In 1991 wilde de Fin Linus Torvalds, die op de universiteit kennis had gemaakt met Unix, ook een soortgelijk besturingssysteem hebben om thuis te gebruiken. Omdat commerciële pakketten te duur waren, was hij genoodzaakt Minix te gebruiken. Al snel voldeed dit besturingssysteem niet meer en besloot hij zijn eigen besturingssysteem te maken. Zijn eerste versie was niet echt een bruikbaar besturingssysteem, maar meer een speeltje voor hackers en programmeurs. Al snel werden er andere ontwikkelaars aangetrokken tot Linus' project en zo groeide Linux al snel uit tot een volledig productief besturingssysteem.
+
+Linux is in feite geen volledig besturingssysteem, maar omvat alleen de kernel. De kernel zorgt er onder andere voor dat software en hardware kunnen samenwerken. Torvalds heeft alleen de kernel gemaakt en heeft als software bestaande GNU-software gebruikt. De kernel werd aangepast zodat de GNU-software hierop kon werken. Daarom wordt het besturingssysteem ook wel GNU/Linux genoemd. In de loop der jaren is er echter steeds meer niet-GNU-software bij gekomen (zoals software onder de BSD-licentie) en is volgens sommigen de naam GNU/Linux dan ook minder van toepassing. Essentiële onderdelen als glibc, fileutils en gcc zijn echter nog steeds GNU. Het volledige systeem wordt meestal kortweg Linux genoemd. Bovendien zijn er systemen (zoals vele embedded systemen) waar boven op de Linuxkernel geen GNU-tools gebruikt worden, waardoor de naam GNU/Linux dan helemaal niet van toepassing is.
+
+In 1992 en 1993 groeide Linux uit tot een volledig functionele kernel en kreeg het ook steeds meer aandacht. Verschillende bedrijven begonnen eigen distributies te ontwikkelen. In 1994 kwamen de eerste nummers uit van het tijdschrift Linux Journal.
+
+Sinds versie 1.0 van de Linuxkernel in 1994 is uitgekomen, is de kernel sterk verbeterd en stabieler geworden. Linux wordt inmiddels door veel bedrijven verkozen boven andere besturingssystemen en dit vooral in de servermarkt.[1] Ook voor embedded toepassingen zoals in mobiele telefoons is Linux populair en verder draaien 92% van de 500 snelste computers ter wereld op Linux. De grootste Linuxgebruiker is Google, met meer dan 100.000 servers[2]. 
+EOF
+
+cat > ~/data/algemeen/linux-toepassingen.txt <<- "EOF"
+Linux werd historisch gezien voornamelijk gebruikt als serversysteem voor bedrijven. Op desktopgebied heeft Linux nog geen sterke positie weten te veroveren: in 2012 had Linux een marktaandeel van 1,18% volgens NetMarketShare.[3][4] Een belangrijke reden hiervoor is dat de meeste computers met een vooraf geïnstalleerde versie van het Windowsbesturingssysteem verkocht worden. Hierdoor is Windows het vertrouwde besturingssysteem voor de meeste eindgebruikers. Het reeds aanwezige besturingssysteem waarvoor men betaalde bij aankoop zelf vervangen of laten vervangen door een Linuxdistributie die afwijkt van de vertrouwde gebruikerservaring is een stap die slechts weinig eindgebruikers zetten. Een mogelijke reden hiervan was dat Linux niet makkelijk geconfigureerd kan worden bij gebruik van minder gangbare hardwareonderdelen. Inmiddels is dit ook sterk verbeterd.
+
+Op servergebied heeft Linux een uitstekende positie: het marktaandeel is aanzienlijk en vooral voor websites is Linux marktleider.[5] Het Linuxmarktaandeel is echter niet eenvoudig af te leiden vanuit de verkoopcijfers van serverhardware, omdat in de regel Windows geïnstalleerd wordt.[6]
+
+De installatie en verwijdering van software wordt in de meeste distributies afgehandeld door speciaal voor dit doeleinde ontwikkelde software: de pakketbeheersoftware. Dit stelt een gebruiker in staat om een keuze te maken uit duizenden pakketten die specifiek voor de betreffende distributie geconfigureerd zijn.
+
+Linux wordt veel gebruikt in combinatie met Apache, MySQL en PHP, en Perl of Python. Deze combinatie van software wordt LAMP genoemd en is de basis van veel internetservers door de eenvoudige verkrijgbaarheid en open structuur.
+
+Omwille van de lage kosten, hoge configureerbaarheid en beschikbaarheid op diverse platformen, wordt Linux ook meer en meer gebruikt op embedded systemen; men spreekt dan van embedded Linux. Mogelijke toepassingen zijn te vinden binnen settopboxen voor televisie, mobiele telefoons, routenavigatiesystemen en mobiele apparaten zoals pda's. Linux wordt zo een concurrent van Symbian voor mobiele telefoons, en een alternatief voor Windows Mobile en Palm OS op handheld devices. Ook Googles besturingssysteem Android (voor smartphones en tablets) is op Linux gebaseerd.
+
+Ook op supercomputers wordt Linux steeds populairder. Op de TOP500-lijst van supercomputers van juni 2012 draaide 90% van de snelste computers op Linux.[7] 
+EOF
+
+cat > ~/data/algemeen/command-line-interface.txt <<- "EOF"
+Wanneer een shell of computerprogramma de command-line-interface afgekort CLI (ook wel opdrachtregelinterface, commandoregel, terminal of command line interface (Engels)) aanbiedt, wil dat zeggen dat de gebruiker regels met tekst die een of meerdere opdrachten bevatten moet invoeren. Nadat de opdracht is uitgevoerd krijgt de gebruiker weer de gelegenheid om de shell of het programma aan te spreken door op de opdrachtregel een nieuwe opdracht in te geven. De CLI geldt als tegenhanger van de bekendere grafische gebruikersomgeving (ofwel de GUI, Graphical User Interface).
+
+Een opdracht of commando wordt doorgaans afgesloten met de 'Enter'-toets. Een opdracht kan bijvoorbeeld een opdracht voor de shell of voor het programma zelf zijn of de naam van een programma dat uitgevoerd moet worden. Bij de meeste opdrachten kunnen allerlei opties meegegeven worden.
+
+Besturingssystemen zoals DOS (command.com) en Unix (bash, csh, tcsh) maken bijvoorbeeld van dit systeem gebruik. Men maakt bij dit type interface voornamelijk gebruik van het toetsenbord om opdrachten op te geven.
+
+Het voordeel hiervan is dat de gebruiker veel gedetailleerdere opdrachten kan geven dan met een GUI mogelijk is en dat ook constructies zoals lussen en conditionele statements (if-then-else) mogelijk zijn. Aan de andere kant heeft een CLI een steilere leercurve dan een GUI, omdat de CLI parate kennis bij de gebruiker vereist.
+
+Om er achter te komen welke commando's beschikbaar zijn, is soms een commando gemaakt dat een overzicht van de mogelijkheden geeft. Zo'n commando is bijvoorbeeld: "man" (van manual, handleiding), "info" (Unix), "help" of "?". Unixprogramma's worden verondersteld een hulpoptie te bieden (meestal -h of --help) met een korte uitleg over de gebruikte parameters.
+
+Naast shells zijn er ook andere programma's die met een CLI (kunnen) werken. De FTP-client en de Telnet-client van Microsoft in Windows 2000 en Windows XP bijvoorbeeld gebruiken een opdrachtregel.
+
+Veel Unix-programma's bieden naast een GUI ook een CLI wat als voordeel heeft dat deze programma's in een shell-script kunnen worden gebruikt om nieuwe functionaliteit te verkrijgen. Programma's met een eigen CLI zijn tamelijk zeldzaam, maar interpreters zoals Python, SWI-Prolog en verschillende SQL-databaseclienten (zoals PostgreSQL en MySQL) zijn hier de uitzondering en bieden wel een eigen CLI. 
+EOF
+
+touch ~/data/personeelszaken/Jansen
+touch ~/data/personeelszaken/Janszen
+touch ~/data/personeelszaken/Janssen
+
+
+echo "Script is uitgevoerd, je kunt de mappen 
+terugvinden in je homefolder" 
